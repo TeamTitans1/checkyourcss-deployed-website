f0a5bc82640bc767134474eedd3430d3617160d7
Min Ho Jang <minho00123@gmail.com>
2024-04-09 18:44:53 +0900

    Refactor: change files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello World</h1>
+    <div className="w-full overflow-hidden bg-primary">
+      <div className="flex justify-center items-center sm:px-16 px-6">
+        <div className="xl:max-w-[1280px] w-full">
+          <Navbar />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-start bg-primary">
+        <div className="xl:max-w-[1280px] w-full">
+          <Hero />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-start sm:px-16 px-6 bg-black">
+        <div className="xl:max-w-[1280px] w-full">
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
