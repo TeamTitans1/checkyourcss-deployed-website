ec0b910888f949af4418f888ce0d3c1e28731f40
Min Ho Jang <minho00123@gmail.com>
2024-04-09 18:40:36 +0900

    Fix: delete some files and change files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        primary: "#ffffff",
+        dimWhite: "#d6d6d6",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
